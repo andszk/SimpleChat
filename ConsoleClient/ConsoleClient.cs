7da9c9e10b68a07638a9a0051d45d30023bd8ad2
andszk <szk.andrzej@gmail.com>
2018-02-05 19:20:21 +0100

    Initial commit

---
+++ ConsoleClient/ConsoleClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class ConsoleClient
+    {
+        private String ip = "127.0.0.1";
+        private int port = 8888;
+        private NetworkStream networkStream;
+
+        public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+        public string IP { get => ip; set => ip = value; }
+        public int Port { get => port; set => port = value; }
+
+        static void Main(string[] args)
+        {
+            ConsoleClient client = new ConsoleClient();
+            client.Connect();
+            client.SendMesseage("Hello World!");
+            Console.ReadKey();
+        }
+
+        private void Connect()
+        {
+            TcpClient connection = new TcpClient(IP, Port);
+            NetworkStream = connection.GetStream();
+        }
+
+        public void SendMesseage(String messeage)
+        {
+            var bytes = Encoding.ASCII.GetBytes(messeage);
+            NetworkStream.Write(bytes, 0, bytes.Length);
+        }
+
+        public int ReadMesseage()
+        {
+            return 1;
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        private String ip = "127.0.0.1";
+        private int port = 8888;
+        private NetworkStream networkStream;
+        private TcpClient client;
+        private TcpListener listener;
+
+
+        public string IP { get => ip; set => ip = value; }
+        public int Port { get => port; set => port = value; }
+        public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+        public TcpClient Client { get => client; set => client = value; }
+        public TcpListener Listener { get => listener; set => listener = value; }
+
+        ~Server()
+        {
+            Listener.Stop();
+            client.Close();
+        }
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.StartService();
+            Console.WriteLine(server.GetMessage());
+            Console.ReadKey();
+        }
+
+        private void StartService()
+        {
+            Listener = new TcpListener(IPAddress.Parse(IP), port);
+            Listener.Start();
+            Client = listener.AcceptTcpClient();
+        }
+
+        public String GetMessage()
+        {
+            NetworkStream = Client.GetStream();
+            byte[] buffer = new byte[Client.ReceiveBufferSize];
+            var bytes = NetworkStream.Read(buffer, 0, buffer.Length);
+            return Encoding.ASCII.GetString(buffer, 0, bytes);
+        }
+    }
+}
